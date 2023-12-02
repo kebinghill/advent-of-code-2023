39938f6f8d387ef9bf316afebf691953478ccb60
Kevin Gil <93kevingil@gmail.com>
2023-12-02 19:42:40 -0300

    1-trebuchet part 2 - it aint much but its honest work

--- 1-trebuchet.js
+++ 1-trebuchet.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const lineReader = require('line-reader');
 
 let array = [];
@@ -6,16 +5,34 @@ let valueArr = [];
 let total = 0;
 
 let numberStrings = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
 };
+
+let initialStr = ['o', 't', 'f', 's', 'e', 'n'];
+let acceptedChar = [
+  'o',
+  'n',
+  'e',
+  't',
+  'w',
+  'h',
+  'r',
+  'f',
+  'u',
+  'i',
+  'v',
+  's',
+  'x',
+  'g',
+];
 
 lineReader.eachLine('data/trebuchet-input.txt', (line, last) => {
   array.push(line);
@@ -27,12 +44,31 @@ lineReader.eachLine('data/trebuchet-input.txt', (line, last) => {
 
       for (let j = 0; j < array[i].length; j++) {
         let character = array[i][j];
-
         if (character >= '0' && character <= '9' && firstNumber === undefined) {
           firstNumber = character;
           secondNumber = character;
         } else if (character >= '0' && character <= '9') {
           secondNumber = character;
+        } else if (initialStr.includes(character)) {
+          let currNumString = character;
+          for (let x = j + 1; x < array[i].length; x++) {
+            if (acceptedChar.includes(array[i][x])) {
+              currNumString += array[i][x];
+            } else {
+              break;
+            }
+            if (currNumString in numberStrings) {
+              if (firstNumber === undefined) {
+                firstNumber = numberStrings[currNumString];
+                break;
+              } else {
+                secondNumber = numberStrings[currNumString];
+                break;
+              }
+            } else if (currNumString.length >= 5) {
+              break;
+            }
+          }
         }
       }
 
@@ -47,19 +83,3 @@ lineReader.eachLine('data/trebuchet-input.txt', (line, last) => {
     }
   }
 });
-
-// fs.readFile('data/trebuchet-input.txt', (err, data) => {
-//   if (err) throw err;
-
-//   let array = [];
-
-//   for (let i = 0; i < data.length - 1; i++) {
-//     let splitStart = data[i];
-//   }
-
-//   console.log(data.indexOf('\n'));
-
-//   array = [...data];
-
-//   console.log(array);
-// });
