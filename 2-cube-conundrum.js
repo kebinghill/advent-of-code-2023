5207c2213ed8886c8e0eaaf4548707ec6e0a9b68
Kevin Gil <93kevingil@gmail.com>
2023-12-05 13:37:32 -0300

    complete part 2 cube conundrum

--- 2-cube-conundrum.js
+++ 2-cube-conundrum.js
@@ -34,30 +34,62 @@ lineReader.eachLine('data/cube-conundrum-input.txt', (line, last) => {
 
   if (last) {
     let count = 1;
+    let powers = [];
     for (let i = 0; i < games.length; i++) {
       const currGame = games[i];
       const rounds = Object.values(currGame);
-      // console.log('-------------------------------');
-      // console.log(`game ${count++}`);
+      console.log('-------------------------------');
+      console.log(`game ${count++}`);
+
+      let maxBlue = 0;
+      let maxRed = 0;
+      let maxGreen = 0;
       for (let j = 0; j < rounds.length - 1; j++) {
         const round = rounds[j];
-        let remainder = Object.keys(round).reduce((a, k) => {
-          a[k] = round[k] - allowedValues[k];
-          return a;
-        }, {});
+        console.log(round);
 
-        const finalVals = Object.values(remainder);
-        let hasPositive = finalVals.some((v) => v > 0);
-
-        if (hasPositive) {
-          break;
+        if (round['green'] && maxGreen < round['green']) {
+          maxGreen = round['green'];
         }
 
-        if (rounds.length - 2 === j) {
-          total += currGame.game;
+        if (round['blue'] && maxBlue < round['blue']) {
+          maxBlue = round['blue'];
         }
+
+        if (round['red'] && maxRed < round['red']) {
+          maxRed = round['red'];
+        }
+
+        if (j === rounds.length - 2) {
+          powers.push(maxBlue * maxRed * maxGreen);
+        }
+
+        // console.log('maxblue', maxBlue);
+        // console.log('maxred', maxRed);
+        // console.log('maxgreen', maxGreen);
+
+        // let remainder = Object.keys(round).reduce((a, k) => {
+        //   a[k] = round[k] - allowedValues[k];
+        //   return a;
+        // }, {});
+
+        // const finalVals = Object.values(remainder);
+        // let hasPositive = finalVals.some((v) => v > 0);
+
+        // if (hasPositive) {
+        //   break;
+        // }
+
+        // if (rounds.length - 2 === j) {
+        //   total += currGame.game;
+        // }
       }
     }
-    console.log(total);
+
+    const sum = powers.reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
+    console.log(sum);
+    // console.log(total);
   }
 });
