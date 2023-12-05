8afcd43bf95b3e81003be3ee740ace5b8880e346
Kevin Gil <93kevingil@gmail.com>
2023-12-05 13:00:13 -0300

    part 1 of cube conundrum complete

--- 2-cube-conundrum.js
+++ 2-cube-conundrum.js
@@ -7,6 +7,7 @@ const allowedValues = {
 };
 
 let games = [];
+let total = 0;
 
 lineReader.eachLine('data/cube-conundrum-input.txt', (line, last) => {
   const game = line.split(/\:|\;/);
@@ -15,7 +16,7 @@ lineReader.eachLine('data/cube-conundrum-input.txt', (line, last) => {
 
   for (let i = 0; i < game.length; i++) {
     if (i === 0) {
-      gameObj.game = game[0];
+      gameObj.game = Number(game[0].split(' ')[1]);
     } else {
       let innerGame = game[i].split(',');
       let innerGameObj = {};
@@ -23,15 +24,40 @@ lineReader.eachLine('data/cube-conundrum-input.txt', (line, last) => {
       for (let j = 0; j < innerGame.length; j++) {
         const val = innerGame[j];
         const value = val.trim().split(' ');
-        innerGameObj[value[1]] = value[0];
+        innerGameObj[value[1]] = Number(value[0]);
       }
 
       gameObj[i] = innerGameObj;
     }
   }
-
   games.push(gameObj);
+
   if (last) {
-    console.log(games);
+    let count = 1;
+    for (let i = 0; i < games.length; i++) {
+      const currGame = games[i];
+      const rounds = Object.values(currGame);
+      // console.log('-------------------------------');
+      // console.log(`game ${count++}`);
+      for (let j = 0; j < rounds.length - 1; j++) {
+        const round = rounds[j];
+        let remainder = Object.keys(round).reduce((a, k) => {
+          a[k] = round[k] - allowedValues[k];
+          return a;
+        }, {});
+
+        const finalVals = Object.values(remainder);
+        let hasPositive = finalVals.some((v) => v > 0);
+
+        if (hasPositive) {
+          break;
+        }
+
+        if (rounds.length - 2 === j) {
+          total += currGame.game;
+        }
+      }
+    }
+    console.log(total);
   }
 });
